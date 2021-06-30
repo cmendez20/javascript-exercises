22af7f4f8e1c37d04c9d8f89b2d3852b1bd5a1dc
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-06-29 23:38:58 -0500

    Completed tempConversion JS exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function (tempInFahr) {
+  return parseFloat((((tempInFahr - 32) * 5) / 9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (tempInCels) {
+  return parseFloat(((tempInCels * 9) / 5 + 32).toFixed(1));
 };
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
