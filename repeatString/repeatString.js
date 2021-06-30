8520384cf63acc95d9c92c808b37c13044eb09cd
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-06-29 20:44:57 -0500

    Completed repeatString & reverseString JS exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, counter) {
+  if (counter < 0) return "ERROR";
 
+  let newStr = "";
+  for (let i = 0; i < counter; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const strArr = str.split("");
+  const reverseArr = [];
 
+  strArr.forEach((letter) => reverseArr.unshift(letter));
+
+  return reverseArr.join("");
 };
 
 module.exports = reverseString;
