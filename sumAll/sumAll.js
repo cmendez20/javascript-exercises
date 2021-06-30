8edf2012cded673a1cc43437cab41cced8494d62
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-06-29 21:46:06 -0500

    Improved code flow of variables

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -2,9 +2,9 @@ const sumAll = function (num1, num2) {
   if (num1 < 0 || num2 < 0) return 'ERROR';
   if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
 
-  let sum = 0;
   const minNum = num1 < num2 ? num1 : num2;
   const maxNum = num2 > num1 ? num2 : num1;
+  let sum = 0;
 
   for (let i = minNum; i <= maxNum; i++) {
     sum += i;
