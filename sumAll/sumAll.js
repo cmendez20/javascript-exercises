cbe876ef9eac33fd4f1310c1e9134bca19c40074
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-06-29 21:41:25 -0500

    Completed sumAll JS exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
 
+  let sum = 0;
+  const minNum = num1 < num2 ? num1 : num2;
+  const maxNum = num2 > num1 ? num2 : num1;
+
+  for (let i = minNum; i <= maxNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 module.exports = sumAll;
