07c2b1728e8eaf691f48504d5d2557a479ccc589
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-09-28 13:43:08 -0500

    Complete palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strArr = [];
+  let isPalindrome = 0;
+  for (let i = 0; i < str.length; i++) {
+    str[i] === '!' || str[i] === '.' || str[i] === ' ' || str[i] === ','
+      ? null
+      : strArr.push(str[i].toLowerCase());
+  }
+  const strArrReverse = strArr.slice().reverse();
+  console.log({ strArr }, { strArrReverse });
 
+  for (let i = 0; i < str.length; i++) {
+    isPalindrome += strArr[i] === strArrReverse[i] ? 1 : -1;
+  }
+
+  console.log({ isPalindrome }, str.length);
+  return isPalindrome === str.length;
 };
 
+// palindromes('ZZZZ car, a man, a maraca.');
+// palindromes('Racecar!');
 module.exports = palindromes;
