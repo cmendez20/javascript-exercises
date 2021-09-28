ce67327918c55afa7dcab31cac3a67e69d6a94ca
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-09-27 21:43:47 -0500

    Complete all calculator exercise tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prevNum, currNum) => prevNum * currNum);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = num;
+  for (let i = num - 1; i > 0; i--) {
+    factorial *= i;
+  }
+
+  return num === 0 ? 1 : factorial;
 };
 
 module.exports = {
@@ -28,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
