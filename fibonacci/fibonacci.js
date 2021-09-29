0f6126b7b2313dd33f464dddee74f2d518c7fbdc
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-09-28 20:11:13 -0500

    Complete fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return 'OOPS';
+  const fibArr = [1];
+  let prevNum = 0;
+  for (let i = 0; i < num; i++) {
+    fibArr.push(prevNum + fibArr[i]);
+    prevNum = fibArr[i];
+  }
+  return fibArr[num - 1];
 };
 
 module.exports = fibonacci;
